f04948b4873372249e83f1b831a928b219fc36f4
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2023-05-02 13:47:52 +0530

    description in coins page added

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -6,7 +6,8 @@ import axios from "axios";
 import { SingleCoin } from "../config/api";
 import { makeStyles } from "tss-react/mui";
 import CoinInfo from "../components/CoinInfo";
-import { Typography } from "@mui/material";
+import { LinearProgress, Typography } from "@mui/material";
+import { numberWithCommas } from "../components/Banner/Carousel";
 
 const CoinPage = () => {
   const { id } = useParams();
@@ -48,9 +49,37 @@ const CoinPage = () => {
         marginBottom: 20,
         fontFamily: "Montserrat",
       },
+      description: {
+        width: "100%",
+        fontFamily: "Montserrat",
+        padding: 25,
+        paddingBottom: 15,
+        paddingTop: 0,
+        textAlign: "justify",
+      },
+      marketData: {
+        alignSelf: "start",
+        padding: 25,
+        paddingTop: 10,
+        width: "100%",
+        //making it responsive
+        [theme.breakpoints.down("md")]: {
+          display: "flex",
+          justifyContent: "space-around",
+        },
+        [theme.breakpoints.down("sm")]: {
+          flexDirection: "column",
+          alignItems: "center",
+        },
+        [theme.breakpoints.down("xs")]: {
+          alignItems: "start",
+        },
+      },
     };
   });
   const { classes } = useStyles();
+
+  if (!coin) return <LinearProgress style={{ backgroundColor: "gold" }} />;
 
   return (
     <div className={classes.container}>
@@ -67,6 +96,46 @@ const CoinPage = () => {
         <Typography variant="subtitle1" className={classes.description}>
           {coin?.description?.en.split(". ")[0]}
         </Typography>
+        <div className={classes.marketData}>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5" style={{ fontFamily: "Montserrat" }}>
+              {coin?.market_cap_rank}
+            </Typography>
+          </span>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Current Price:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5" style={{ fontFamily: "Montserrat" }}>
+              {" "}
+              {currentSymbol}{" "}
+              {numberWithCommas(
+                coin?.market_data?.current_price[currentCurrency.toLowerCase()]
+              )}
+            </Typography>
+          </span>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Market Cap:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant="h5" style={{ fontFamily: "Montserrat" }}>
+              {" "}
+              {currentSymbol}{" "}
+              {numberWithCommas(
+                coin?.market_data?.market_cap[currentCurrency.toLowerCase()]
+                  .toString()
+                  .slice(0, -6)
+              )}
+              M
+            </Typography>
+          </span>
+        </div>
       </div>
 
       {/* chart */}
--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { CryptoContext } from "../CryptoContext";
+import { HistoricalChart } from "../config/api";
+import axios from "axios";
+import { CircularProgress, createTheme } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import { makeStyles } from "tss-react/mui";
 
-const CoinInfo = () => {
-  return <div>CoinInfo</div>;
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const CoinInfo = ({ coin }) => {
+  const [historicalData, setHistoricalData] = useState([]);
+  const [days, setDays] = useState(1);
+  const { currentCurrency } = useContext(CryptoContext);
+
+  const fetchHistoricalData = async () => {
+    const { data } = await axios.get(
+      HistoricalChart(coin?.id, days, currentCurrency)
+    );
+    setHistoricalData(data.prices);
+  };
+
+  useEffect(() => {
+    fetchHistoricalData();
+  }, [currentCurrency, days]);
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+  const useStyles = makeStyles()((theme) => {
+    return {
+      container: {
+        width: "75%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 25,
+        padding: 40,
+        [theme.breakpoints.down("md")]: {
+          width: "100%",
+          marginTop: 0,
+          padding: 20,
+          paddingTop: 0,
+        },
+      },
+    };
+  });
+  const { classes } = useStyles();
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.container}>
+        {/* chart */}
+        {!historicalData ? (
+          <CircularProgress
+            style={{ color: "gold" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicalData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()}PM`
+                      : `${date.getHours()}:${date.getMinutes()}AM`;
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+                datasets: [
+                  {
+                    data: historicalData.map((coin) => coin[1]),
+                  },
+                ],
+              }}
+            />
+          </>
+        )}
+
+        {/* buttons */}
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default CoinInfo;
